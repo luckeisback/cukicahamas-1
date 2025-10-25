5625b85cbec5d9776d787407b32874ea5ee8d683
luckeisback <jeanben1999@hotmail.com>
2025-10-25 19:04:45 +0000

    Initial commit: ruby project

---
+++ README.md
@@ -0,0 +1,17 @@
+# cukicahamas-1
+
+A simple Ruby project.
+
+## Description
+This is an auto-generated project demonstrating basic ruby functionality.
+
+## Running
+```bash
+# See the main file: main.rb
+```
+
+## Language
+- Ruby
+
+## License
+MIT
---
+++ main.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# cukicahamas-1 - Ruby Implementation
+# A simple Hello World program
+
+def main
+  puts "Hello, World!"
+  puts "Welcome to cukicahamas-1"
+  
+  # Simple calculation
+  numbers = [1, 2, 3, 4, 5]
+  total = numbers.sum
+  puts "Sum of numbers: #{total}"
+end
+
+main
